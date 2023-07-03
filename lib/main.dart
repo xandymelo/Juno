19c9db3e21e47b56596718d45bec2d9edd3fd046
Helen <helenalves.02@gmail.com>
2023-07-02 22:13:23 -0300

    New Sign Up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:juno/src/screens/rides_and_companies/ui/general_tabview.dart';
+import 'package:juno/src/screens/rides_and_companies/ui/perfect_displacement_tabview.dart';
+import 'package:juno/src/screens/rides_and_companies/ui/perfect_displacements_screen.dart';
+import 'package:juno/src/screens/rides_and_companies/ui/rides_and_companies_screen.dart';
 
 import 'src/app/app_initialization.dart';
 import 'src/app/app_widget.dart';
@@ -6,5 +10,19 @@ import 'src/app/app_widget.dart';
 void main() {
   AppInitialization.init();
 
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        '/': (context) => PerfectDisplacementsScreen(),
+        '/WelcomeScreen': (context) => RidesAndCompaniesScreen(),
+      },
+    );
+  }
 }
