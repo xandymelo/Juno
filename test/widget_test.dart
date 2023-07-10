4d28547f6a5f13efaeddde0ab5d14f4baa7a0857
abilio <abilio2001abilio@gmail.com>
2023-07-10 14:58:36 -0300

    atualizaçoes do H3
    atualizando e tentando plotar o gráfico visual do h3

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:juno/main.dart';
+import 'package:juno/src/app/app_widget.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
