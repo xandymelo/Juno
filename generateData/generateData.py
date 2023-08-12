03528ca3cf71f867634de9cc1a877200c7a011af
Marília Costa <marilia.gouveia.costa@gmail.com>
2023-08-12 10:46:51 -0300

    Atualizações no Código Gerador de Dados
    Atualização no código gerador de dados para:
    - Gerar o endereço separado em CEP, bairro, rua, município, tipo de subdivisão e subdivisão;
    - Selecionar um carro para carona com a marca e o modelo reais;
    - Calcular a distância entre dois ponto com base no nome das localizações;
    
    Inclusão de uma pasta ("generateData\data") para salvar os CSVs criados contendo os dados que foram gerados;
    
    Obs.: Os CSVs , "TabelaDeslocamento", "TabelaEndereco", "TabelaSigaa",  "TabelaUsuarios" e  "TabelaVeiculos" possuem 50.000 dados, já o CSV  "TabelaPassageiros" possui 100.000 e foi necessário mais de 8 horas para gerá-los.

--- generateData/generateData.py
+++ generateData/generateData.py
@@ -1,10 +1,15 @@
 import datetime
 import random
 import string
+import csv
 import pandas as pd
-from enums.Cursos import cursos
+from geopy.distance import geodesic as GD
+from geopy.geocoders import Nominatim
+import requests
+import urllib.parse
 from faker import Faker
-from mock.municipiosBairros import municipios_pe, bairros_por_municipio
+from enums.Cursos import cursos
+
 
 
 def generateCursoData():
@@ -16,7 +21,7 @@ def generateCursoData():
         'Turno': turno
     }
     dfCursos = pd.DataFrame(dataCursos)
-    dfCursos.to_csv('TabelaCursos.csv', index=False)
+    dfCursos.to_csv('generateData\data\TabelaCursos.csv', index=False)
 
 def generateSigaaData(QTD_DADOS):
     sigaaID = list(range(1, QTD_DADOS + 1))
@@ -31,40 +36,70 @@ def generateSigaaData(QTD_DADOS):
         'Periodo': periodo
         }
     dfSigaa = pd.DataFrame(dataSigaa)
-    dfSigaa.to_csv('TabelaSigaa.csv', index=False)
+    dfSigaa.to_csv('generateData\data\TabelaSigaa.csv', index=False)
 
 def generateEnderecoData(QTD_DADOS):
-    fake = Faker('pt_BR')
     id = list(range(1, QTD_DADOS + 1))
-    municipio = [random.choice(municipios_pe)for _ in range(QTD_DADOS)]
-    bairro = [random.choice(bairros_por_municipio[m])for m in municipio]
-    rua = [fake.street_name() for _ in range(QTD_DADOS)]
-    numero = [random.randint(1, 9999) for _ in range(QTD_DADOS)]
-    complemento = [random.choice(["casa", "apartamento"])for _ in range(QTD_DADOS)]
+
+    enderecoscsv = open("generateData\enums\lista_enderecos2.csv", encoding="utf8")
+    enderecos = list(csv.reader(enderecoscsv, delimiter=","))
+
+    cep = []
+    rua = []
+    bairro = []
+    tipoSubdivisao = []
+    subdivisao = []
+    municipio = []
+    estado = []
+    
+    for endereco in range(1, QTD_DADOS + 1):
+        enderecoID = random.randint(1, len(enderecos) - 1)
+        cep.append(enderecos[enderecoID][1])
+        rua.append(enderecos[enderecoID][2])
+        bairro.append(enderecos[enderecoID][3])
+        tipoSubdivisao.append(enderecos[enderecoID][4])
+        subdivisao.append(enderecos[enderecoID][5])
+        municipio.append(enderecos[enderecoID][6])
+        estado.append(enderecos[enderecoID][7])
+    
+    complemento = [random.choice(["Casa", "Apartamento"])for _ in range(QTD_DADOS)]
 
     dataEndereco = {
         "Id": id,
+        'CEP': cep,
+        'Rua': rua,
+        'Bairro': bairro,
+        'Tipo de Subdivisao': tipoSubdivisao,
+        'Subdivisao': subdivisao,
         "Municipio": municipio,
-        "Bairro": bairro,
-        "Rua": rua,
-        "Numero": numero,
+        "Estado": estado,
         "Complemento": complemento
         }
     dfEndereco = pd.DataFrame(dataEndereco)
-    dfEndereco.to_csv("TabelaEndereco.csv", index=False)
+    dfEndereco.to_csv("generateData\data\TabelaEndereco.csv", index=False)
 
 def generateUsuarioData(QTD_DADOS_USUARIO, QTD_DADOS_ENDERECO):
     fake = Faker('pt_BR')
     usuarioID = list(range(1, QTD_DADOS_USUARIO + 1))
     sigaaID = list(range(1, QTD_DADOS_USUARIO + 1))
     enderecoID = list(range(1, QTD_DADOS_ENDERECO + 1))
-    nome = [fake.name() for _ in range(QTD_DADOS_USUARIO)]
-    sobrenome = [fake.last_name() for _ in range(QTD_DADOS_USUARIO)] 
+    nome = [fake.first_name_female() for _ in range(QTD_DADOS_USUARIO)]
+    sobrenome = []
+
+    for quant_usuario in range(1, QTD_DADOS_USUARIO + 1):
+        sobrenome1 = fake.last_name()
+        sobrenome2 = fake.last_name()
+
+        while sobrenome1 == sobrenome2:
+            sobrenome2 = fake.last_name()
+
+        sobrenome.append(str(sobrenome1) + " " + str(sobrenome2))
+
     cpf = [fake.cpf() for _ in range(QTD_DADOS_USUARIO)]
     senha = [fake.password() for _ in range(QTD_DADOS_USUARIO)]
     telefone = ['81' + fake.numerify('#######') for _ in range(QTD_DADOS_USUARIO)]  # Gera telefones fictícios com DDD 81
     email = [f"{n.lower().replace(' ', '')}.{s.lower().replace(' ', '')}@ufrpe.br" for n, s in zip(nome, sobrenome)]  # Gera emails fictícios
-    nascimento = [fake.date_of_birth(minimum_age=17, maximum_age=70).strftime('%Y-%m-%d 00:00:00') for _ in range(QTD_DADOS_USUARIO)]  # Gera datas de nascimento fictícias com pelo menos 17 anos
+    nascimento = [fake.date_of_birth(minimum_age=17, maximum_age=70).strftime('%Y-%m-%d') for _ in range(QTD_DADOS_USUARIO)]  # Gera datas de nascimento fictícias com pelo menos 17 anos
 
     data = {
         'ID': usuarioID,
@@ -82,7 +117,7 @@ def generateUsuarioData(QTD_DADOS_USUARIO, QTD_DADOS_ENDERECO):
     dfUsuarios = pd.DataFrame(data)
 
     # Exportar o dataframe para um arquivo excel
-    dfUsuarios.to_csv('TabelaUsuarios.csv', index=False)
+    dfUsuarios.to_csv('generateData\data\TabelaUsuarios.csv', index=False)
 
 def generate_license_plate():
     letters = random.choices(string.ascii_uppercase, k=3)
@@ -95,10 +130,20 @@ def generateVeiculoData(QTD_DADOS, QTD_DADOS_USUARIO):
     usuarioID = [random.choice(usuariosIds) for _ in range(QTD_DADOS)] 
     tipo = [random.randint(1,2) for _ in range(QTD_DADOS)]
     placa = [generate_license_plate() for _ in range(QTD_DADOS)]
-    marcas = ["Ford", "Chevrolet", "Volkswagen", "Toyota", "Honda", "Hyundai", "Nissan", "Renault", "BMW", "Mercedes-Benz"]
-    marca = [random.choice(marcas) for _ in range(QTD_DADOS)]
-    modelos = ["Gol", "Onix", "Corolla", "Civic", "HB20", "Renegade", "Uno", "Palio", "Fiesta", "Cruze"]
-    modelo = [random.choice(modelos) for _ in range(QTD_DADOS)]
+
+    carroscsv = open("generateData\enums\lista_carros.csv", encoding="utf8")
+    carros = list(csv.reader(carroscsv, delimiter=","))
+
+    ano = []
+    marca = []
+    modelo = []
+
+    for carro in range(1, QTD_DADOS + 1):
+        carroID = random.randint(1, len(carros) - 1)
+        ano.append(carros[carroID][0])
+        marca.append(carros[carroID][1])
+        modelo.append(carros[carroID][2])
+
     cores = ["Preto", "Branco", "Prata", "Cinza", "Azul", "Vermelho", "Verde", "Amarelo"]
     cor = [random.choice(cores) for _ in range(QTD_DADOS)]
     passageiros = [random.randint(1, 4) for _ in range(QTD_DADOS)]
@@ -107,13 +152,45 @@ def generateVeiculoData(QTD_DADOS, QTD_DADOS_USUARIO):
         'UsuarioID': usuarioID,
         'Tipo': tipo,
         'Placa': placa,
+        'Ano': ano,
         'Marca': marca,
         'Modelo': modelo,
         'Cor': cor,
         'Passageiros': passageiros
     }
     dfVeiculos = pd.DataFrame(dataVeiculos)
-    dfVeiculos.to_csv('TabelaVeiculos.csv', index=False)
+    dfVeiculos.to_csv('generateData\data\TabelaVeiculos.csv', index=False)
+
+def generateHora():
+    horaIds = list(range(1, 1441))
+    horario = []
+    turno = []
+    
+    turno_madrugada = [23, 24, 0, 1, 2, 3, 4]
+    turno_manha = [5, 6, 7, 8, 9, 10, 11]
+    turno_tarde = [12, 13, 14, 15, 16, 17]
+    turno_noite = [18, 19, 20, 21, 22]
+
+    for hora in range(00, 24):
+        for minuto in range(00, 60):
+            horario.append([f'{str(hora).zfill(2)}:{str(minuto).zfill(2)}:00'])
+            if hora in turno_madrugada:
+                turno.append('Madrugada')
+            elif hora in turno_manha:
+                turno.append('Manhã')
+            elif hora in turno_tarde:
+                turno.append('Tarde')
+            elif hora in turno_noite:
+                turno.append('Noite')
+
+    dataHora = {
+        'HoraID': horaIds,
+        'Hora': horario,
+        'Turno': turno
+    }   
+    dfHora = pd.DataFrame(dataHora)
+    dfHora.to_csv('generateData\data\TabelaHora.csv', index=False)
+
 
 def generateDeslocamentosData(QTD_DADOS, QTD_DADOS_ENDERECO, QTD_DADOS_USUARIO, QTD_DADOS_VEICULO):
     id = list(range(1, QTD_DADOS + 1))
@@ -121,24 +198,69 @@ def generateDeslocamentosData(QTD_DADOS, QTD_DADOS_ENDERECO, QTD_DADOS_USUARIO, 
     usuarioID = [random.choice(usuarioIds) for _ in range(QTD_DADOS)]
     veiculoIds = list(range(1, QTD_DADOS_VEICULO+ 1))
     veiculoID = [random.choice(veiculoIds) for _ in range(QTD_DADOS)]
-    horaSaida = [f'{random.randint(0, 24)}:{random.randint(0, 60)}:00' for _ in range(QTD_DADOS)]
+    dataID = [random.randint(20080101, 20230828) for _ in range(QTD_DADOS)]
+    horaIds = list(range(1, 1441))
+    horaID = [random.choice(horaIds) for _ in range(QTD_DADOS)]
     enderecoIds = list(range(1, QTD_DADOS_ENDERECO + 1))
-    origem = [random.choice(enderecoIds) for _ in range(QTD_DADOS)]
-    destino = [random.choice(enderecoIds) for _ in range(QTD_DADOS)]
+    origemID = []
+    destinoID = []
+
+    enderecoscsv = open("generateData\data\TabelaEndereco.csv", encoding="utf8")
+    enderecos = list(csv.reader(enderecoscsv, delimiter=","))
+
+    geoLocalizador = Nominatim(user_agent="my_user_agent")
+
+    while len(origemID) < QTD_DADOS:
+
+        origemEscolhida = random.choice(enderecoIds)
+        origemCompleta = str(enderecos[origemEscolhida][2])  + ", " + str(enderecos[origemEscolhida][1])
+
+        dadosLocalOrigem = geoLocalizador.geocode(origemCompleta)
+
+        if dadosLocalOrigem != None:
+            origemID.append(origemEscolhida)
+            
+    while len(destinoID) < QTD_DADOS:
+
+        destinoEscolhido = random.choice(enderecoIds)
+        destinoCompleto = str(enderecos[destinoEscolhido][2])  + ", " + str(enderecos[destinoEscolhido][1])
+
+        dadosLocalDestino = geoLocalizador.geocode(destinoCompleto)
+
+        if origemID[len(destinoID)] != destinoEscolhido and dadosLocalDestino != None:
+            destinoID.append(destinoEscolhido)
+
     status = [random.randint(1,2) for _ in range(QTD_DADOS)]
     vagasDisponiveis = [random.randint(0,4) for _ in range(QTD_DADOS)]
+    distancia = []
+
+    for localizacoes in range(0, len(id)):
+        origem = str(enderecos[origemID[localizacoes]][2])  + ", " + str(enderecos[origemID[localizacoes]][1])
+        destino = str(enderecos[destinoID[localizacoes]][2]) + ", " + str(enderecos[destinoID[localizacoes]][1])
+
+        dadosLocalOrigem = geoLocalizador.geocode(origem)
+        dadosLocalDestino = geoLocalizador.geocode(destino)
+
+        coordenadasorigem = (dadosLocalOrigem.latitude, dadosLocalOrigem.longitude)
+        coordenadasdestino = (dadosLocalDestino.latitude, dadosLocalDestino.longitude)
+
+        distancia.append(round(GD(coordenadasorigem,coordenadasdestino).km, 2))
+
     dataDeslocamento = {
         "id": id,
         "UsuarioID": usuarioID,
         "VeiculoID": veiculoID,
-        "HoraSaida": horaSaida,
-        "Origem": origem,
-        "Destino": destino,
+        'DataID': dataID,
+        "HorarioID": horaID,
+        "OrigemID": origemID,
+        "DestinoID": destinoID,
         "Status": status,
-        "VagasDisponiveis": vagasDisponiveis
+        "VagasDisponiveis": vagasDisponiveis,
+        "Distancia": distancia
     }
+
     dfDeslocamento = pd.DataFrame(dataDeslocamento)
-    dfDeslocamento.to_csv('TabelaDeslocamentos.csv', index=False)
+    dfDeslocamento.to_csv('generateData\data\TabelaDeslocamentos.csv', index=False)
 
 def generatePassageirosData(QTD_DADOS, QTD_DADOS_USUARIO, QTD_DADOS_DESLOCAMENTOS):
     id = list(range(1, QTD_DADOS + 1))
@@ -152,19 +274,22 @@ def generatePassageirosData(QTD_DADOS, QTD_DADOS_USUARIO, QTD_DADOS_DESLOCAMENTO
         'deslocamentoId': deslocamentoId
     }
     dfPassageiros = pd.DataFrame(dataPassageiros)
-    dfPassageiros.to_csv('TabelaPassageiros.csv', index=False)
-
-
+    dfPassageiros.to_csv('generateData\data\TabelaPassageiros.csv', index=False)
 
 if __name__ == '__main__':
+
     qtd_dados_usuario = int(input('Escolha a quantidade de Usuários:'))
     qtd_dados_endereco = int(input('Escolha a quantidade de Endereços:'))
     qtd_dados_veiculos = int(input('Escolha a quantidade de Veiculos:'))
-    qtd_dados_deslocamento = int(input('Escolha a quantidade de deslocamentos:'))
+    qtd_dados_deslocamento = int(input('Escolha a quantidade de Deslocamentos:'))
+
     generateCursoData()
     generateEnderecoData(qtd_dados_endereco)
     generateSigaaData(qtd_dados_usuario)
     generateUsuarioData(qtd_dados_usuario, qtd_dados_endereco)
     generateVeiculoData(QTD_DADOS=qtd_dados_veiculos, QTD_DADOS_USUARIO=qtd_dados_usuario)
+    generateHora()
     generateDeslocamentosData(QTD_DADOS=qtd_dados_deslocamento, QTD_DADOS_ENDERECO=qtd_dados_endereco, QTD_DADOS_USUARIO=qtd_dados_usuario, QTD_DADOS_VEICULO=qtd_dados_veiculos)
     generatePassageirosData(QTD_DADOS=qtd_dados_deslocamento, QTD_DADOS_USUARIO= qtd_dados_usuario, QTD_DADOS_DESLOCAMENTOS=qtd_dados_deslocamento)
+    
+    print("Os dados foram gerados com sucesso!")
