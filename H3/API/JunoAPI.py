c141c304dd87220e027a7d48746e2d4a2ea24832
abilio <abilio2001abilio@gmail.com>
2023-08-27 14:40:40 -0300

    Criaçao da API
    Criaçao da API para as consultas

---
+++ H3/API/JunoAPI.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, HTTPException
+from sqlalchemy.sql import text
+import databases
+
+Juno = FastAPI()
+
+DATABASE_URL = "mysql+mysqlconnector://root:abilio2012@localhost:3306/junodwpopulado"
+
+database = databases.Database(DATABASE_URL)
+
+@Juno.on_event("startup")
+async def startup():
+    await database.connect()
+
+@Juno.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+@Juno.get("/")
+def home():
+    return "API Juno"
+
+@Juno.get("/alunos")
+async def read_alunos():
+    # consulta
+    query = text("SELECT aluno_id, nome, sobrenome, cpf FROM dimaluno")
+    alunos = await database.fetch_all(query=query)
+    return alunos
+
+# teste de consulta
+@Juno.get("/aluno")
+async def read_aluno(aluno_id: int):
+    print("Valor de aluno_id:", aluno_id)
+    query = text("SELECT aluno_id, nome, sobrenome, cpf FROM dimaluno WHERE aluno_id = :aluno_id")
+    aluno = await database.fetch_all(query=query, values={"aluno_id": aluno_id})
+    return aluno
+
+
+
