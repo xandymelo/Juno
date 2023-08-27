ea54e06089986b4113d0dd19371146a0d0df5a32
Alexandre Melo <xandymelo@outlook.com.br>
2023-08-27 20:11:12 -0300

    api - corrigindo erro de conexão com o banco

--- H3/API/JunoAPI.py
+++ H3/API/JunoAPI.py
@@ -6,34 +6,34 @@ Juno = FastAPI()
 
 DATABASE_URL = "mysql+mysqlconnector://root:abilio2012@localhost:3306/junodwpopulado"
 
-database = databases.Database(DATABASE_URL)
+# database = databases.Database(DATABASE_URL)
 
-@Juno.on_event("startup")
-async def startup():
-    await database.connect()
+# @Juno.on_event("startup")
+# async def startup():
+#     await database.connect()
 
-@Juno.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
+# @Juno.on_event("shutdown")
+# async def shutdown():
+#      await database.disconnect()
 
 @Juno.get("/")
 def home():
     return "API Juno"
 
-@Juno.get("/alunos")
-async def read_alunos():
-    # consulta
-    query = text("SELECT aluno_id, nome, sobrenome, cpf FROM dimaluno")
-    alunos = await database.fetch_all(query=query)
-    return alunos
+# @Juno.get("/alunos")
+# async def read_alunos():
+#     # consulta
+#     query = text("SELECT aluno_id, nome, sobrenome, cpf FROM dimaluno")
+#     alunos = await database.fetch_all(query=query)
+#     return alunos
 
 # teste de consulta
-@Juno.get("/aluno")
-async def read_aluno(aluno_id: int):
-    print("Valor de aluno_id:", aluno_id)
-    query = text("SELECT aluno_id, nome, sobrenome, cpf FROM dimaluno WHERE aluno_id = :aluno_id")
-    aluno = await database.fetch_all(query=query, values={"aluno_id": aluno_id})
-    return aluno
+# @Juno.get("/aluno")
+# async def read_aluno(aluno_id: int):
+#     print("Valor de aluno_id:", aluno_id)
+#     query = text("SELECT aluno_id, nome, sobrenome, cpf FROM dimaluno WHERE aluno_id = :aluno_id")
+#     aluno = await database.fetch_all(query=query, values={"aluno_id": aluno_id})
+#     return aluno
 
 
 
