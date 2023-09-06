c6ecce60200e1dfbd44ffec74a87904988de07a2
Marília Costa <marilia.gouveia.costa@gmail.com>
2023-09-06 00:00:10 -0300

    Ajuste Nome do Bairro

--- lib/main.dart
+++ lib/main.dart
@@ -34,21 +34,21 @@ class MyApp extends StatelessWidget {
 }
 
 Future<void> createDefaultData() async {
-  final endereco = Endereco(bairro: 'Candeias', id: 1, complemento: 'Casa', municipio: 'Jaboatão dos Guararapes', numero: 7535, rua: 'rua Padre Nestor de Alencar');
+  final endereco = Endereco(bairro: 'CANDEIAS', id: 1, complemento: 'Casa', municipio: 'Jaboatão dos Guararapes', numero: 7535, rua: 'rua Padre Nestor de Alencar');
   await EnderecoDAO.save(endereco);
-  final endereco2 = Endereco(bairro: 'Prado', id: 2, complemento: 'Prédio', municipio: 'Recife', numero: 7535, rua: 'rua Padre Nestor de Alencar');
+  final endereco2 = Endereco(bairro: 'PRADO', id: 2, complemento: 'Prédio', municipio: 'Recife', numero: 7535, rua: 'rua Padre Nestor de Alencar');
   await EnderecoDAO.save(endereco2);
-  final endereco3 = Endereco(bairro: 'Torrões', id: 3, complemento: 'Prédio', municipio: 'Recife', numero: 7535, rua: 'rua Padre Nestor de Alencar');
+  final endereco3 = Endereco(bairro: 'TORROES', id: 3, complemento: 'Prédio', municipio: 'Recife', numero: 7535, rua: 'rua Padre Nestor de Alencar');
   await EnderecoDAO.save(endereco3);
-  final endereco4 = Endereco(bairro: 'Candeias', id: 4, complemento: 'Casa', municipio: 'Jaboatão dos Guararapes', numero: 7535, rua: 'rua Padre Nestor de Alencar');
+  final endereco4 = Endereco(bairro: 'CANDEIAS', id: 4, complemento: 'Casa', municipio: 'Jaboatão dos Guararapes', numero: 7535, rua: 'rua Padre Nestor de Alencar');
   await EnderecoDAO.save(endereco4);
-  final endereco5 = Endereco(bairro: 'Boa Viagem', id: 5, complemento: 'Prédio', municipio: 'Recife', numero: 7535, rua: 'rua Padre Nestor de Alencar');
+  final endereco5 = Endereco(bairro: 'BOA VIAGEM', id: 5, complemento: 'Prédio', municipio: 'Recife', numero: 7535, rua: 'rua Padre Nestor de Alencar');
   await EnderecoDAO.save(endereco5);
   final endereco6 = Endereco(bairro: 'IPSEP', id: 6, complemento: 'Prédio', municipio: 'Recife', numero: 7535, rua: 'rua Padre Nestor de Alencar');
   await EnderecoDAO.save(endereco6);
-  final endereco7 = Endereco(bairro: 'Candeias', id: 7, complemento: 'Casa', municipio: 'Jaboatão dos Guararapes', numero: 7535, rua: 'rua Padre Nestor de Alencar');
+  final endereco7 = Endereco(bairro: 'CANDEIAS', id: 7, complemento: 'Casa', municipio: 'Jaboatão dos Guararapes', numero: 7535, rua: 'rua Padre Nestor de Alencar');
   await EnderecoDAO.save(endereco7);
-  final endereco8 = Endereco(bairro: 'Candeias', id: 8, complemento: 'Casa', municipio: 'Jaboatão dos Guararapes', numero: 7535, rua: 'rua Padre Nestor de Alencar');
+  final endereco8 = Endereco(bairro: 'BONGI', id: 8, complemento: 'Casa', municipio: 'Jaboatão dos Guararapes', numero: 7535, rua: 'rua Padre Nestor de Alencar');
   await EnderecoDAO.save(endereco8);
 
   // EnderecoDAO.findAll().then((value) => print(value));
@@ -65,69 +65,103 @@ Future<void> createDefaultData() async {
   final usuario = User(
       cpf: '097.882.964-67',
       id: 1,
-      nome: 'Morgada',
+      nome: 'Roberta',
       sigaaId: 1,
       enderecoId: 1,
-      email: ' morgana.brito@outlook.com.br',
+      email: ' roberya.brito@outlook.com.br',
       senha: 'teste123',
       sobrenome: 'Brito',
       telefone: '81997570531',
       dataNascimento: '1998-03-30',
       imageUrl: 'https://i.ibb.co/qNSSN87/fotor-ai-20230905202730.jpg',
-      hasAccount: true);
+      hasAccount: false);
   await UserDAO.save(usuario);
   final segundoUsuario = User(
-      cpf: '097.882.964-68',
+      cpf: '124.785.954-68',
       id: 2,
       nome: 'Maria Eliza',
-      sigaaId: 1,
-      enderecoId: 1,
+      sigaaId: 2,
+      enderecoId: 2,
       email: 'mariaeliza@outlook.com.br',
-      senha: 'teste123',
+      senha: 'senhasegura123',
       sobrenome: 'Melo',
       telefone: '81997570531',
-      dataNascimento: '1998-03-30',
+      dataNascimento: '2000-03-20',
       imageUrl: 'https://i.ibb.co/r3bJZDR/profile1.jpg',
-      hasAccount: false);
+      hasAccount: true);
   await UserDAO.save(segundoUsuario);
+    final terceiroUsuario = User(
+      cpf: '155.865.975-68',
+      id: 3,
+      nome: 'Talita',
+      sigaaId: 3,
+      enderecoId: 3,
+      email: 'talitapereira@outlook.com.br',
+      senha: 'minhasenhasecreta123',
+      sobrenome: 'Pereira',
+      telefone: '81997570531',
+      dataNascimento: '1997-04-30',
+      imageUrl: 'https://i.ibb.co/5WDcVfv/profile-picture1.jpg',
+      hasAccount: true);
+  await UserDAO.save(terceiroUsuario);
+    final quartoUsuario = User(
+      cpf: '875.954.354-68',
+      id: 4,
+      nome: 'Morgana',
+      sigaaId: 4,
+      enderecoId: 4,
+      email: 'mariaeliza@outlook.com.br',
+      senha: 'teste123',
+      sobrenome: 'Gouveia',
+      telefone: '81997570531',
+      dataNascimento: '1998-03-14',
+      imageUrl: 'https://i.ibb.co/r3bJZDR/profile1.jpg',
+      hasAccount: true);
+  await UserDAO.save(quartoUsuario);
   // UserDAO.findAll().then((value) => print(value));
-  final veiculo = Veiculo(id: 1, cor: 'branco', marca: 'Hyundai', modelo: 'HB20', placa: 'PCM9F00', qtdPassageiros: 4, tipo: 0, usuarioId: 1);
-  await VeiculoDAO.save(veiculo);
   // VeiculoDAO.findAll().then((value) => print(value));
+
+  // DeslocamentoDAO.findAll().then((value) => print(value));
+
+  final veiculo = Veiculo(id: 1, cor: 'Branco', marca: 'Hyundai', modelo: 'HB20', placa: 'PCM9F00', qtdPassageiros: 4, tipo: 0, usuarioId: 1);
+  await VeiculoDAO.save(veiculo);
+  final segundoVeiculo = Veiculo(id: 2, cor: 'Preto', marca: 'Volkswagen', modelo: 'Gol', placa: 'YGD9F23', qtdPassageiros: 4, tipo: 0, usuarioId: 1);
+  await VeiculoDAO.save(segundoVeiculo);
+  final terceiroVeiculo = Veiculo(id: 2, cor: 'Prata', marca: 'Fiat', modelo: 'Uno', placa: 'TDS9T32', qtdPassageiros: 4, tipo: 0, usuarioId: 1);
+  await VeiculoDAO.save(terceiroVeiculo);
+
   final deslocamento = Deslocamento(destinoId: 1, horaSaida: "13:00", id: 1, origemId: 1, status: 0, vagasDisponiveis: 4, veiculoId: 1, vagas: 4);
   DeslocamentoDAO.save(deslocamento);
-  // DeslocamentoDAO.findAll().then((value) => print(value));
+  final segundoDeslocamento = Deslocamento(destinoId: 2, horaSaida: "14:00", id: 2, origemId: 1, status: 0, vagasDisponiveis: 1, veiculoId: 2, vagas: 1);
+  DeslocamentoDAO.save(segundoDeslocamento);
+ final terceiroDeslocamento = Deslocamento(destinoId: 3, horaSaida: "14:00", id: 3, origemId: 1, status: 0, vagasDisponiveis: 4, veiculoId: 3, vagas: 4);
+  DeslocamentoDAO.save(terceiroDeslocamento);
+ final quartoDeslocamento = Deslocamento(destinoId: 4, horaSaida: "21:00", id: 4, origemId: 1, status: 0, vagasDisponiveis: 4, veiculoId: 1, vagas: 4);
+  DeslocamentoDAO.save(quartoDeslocamento);
+ final quintoDeslocamento = Deslocamento(destinoId: 5, horaSaida: "12:00", id: 5, origemId: 1, status: 0, vagasDisponiveis: 4, veiculoId: 2, vagas: 4);
+  DeslocamentoDAO.save(quintoDeslocamento);
+ final sextoDeslocamento = Deslocamento(destinoId: 6, horaSaida: "11:00", id: 6, origemId: 1, status: 0, vagasDisponiveis: 4, veiculoId: 3, vagas: 4);
+  DeslocamentoDAO.save(sextoDeslocamento);
+final setimoDeslocamento = Deslocamento(destinoId: 7, horaSaida: "13:00", id: 7, origemId: 3, status: 0, vagasDisponiveis: 4, veiculoId: 1, vagas: 4);
+  DeslocamentoDAO.save(setimoDeslocamento);
+final oitavoDeslocamento = Deslocamento(destinoId: 8, horaSaida: "08:00", id: 8, origemId: 3, status: 0, vagasDisponiveis: 4, veiculoId: 2, vagas: 4);
+  DeslocamentoDAO.save(oitavoDeslocamento);
+
   final passageirosDeslocamento = PassageirosDeslocamento(id: 1, usuarioId: 1, deslocamentoId: 1, tipo: 1);
   PassageirosDeslocamentoDAO.save(passageirosDeslocamento);
-  final segundoVeiculo = Veiculo(id: 2, cor: 'preto', marca: 'SHINERAY', modelo: 'jet50', placa: 'PCM9F01', qtdPassageiros: 1, tipo: 1, usuarioId: 1);
-  await VeiculoDAO.save(segundoVeiculo);
-  final segundoDeslocamento = Deslocamento(destinoId: 1, horaSaida: "14:00", id: 2, origemId: 1, status: 0, vagasDisponiveis: 1, veiculoId: 2, vagas: 1);
-  DeslocamentoDAO.save(segundoDeslocamento);
-  final segundoPassageirosDeslocamento = PassageirosDeslocamento(id: 2, usuarioId: 1, deslocamentoId: 2, tipo: 1);
+  final segundoPassageirosDeslocamento = PassageirosDeslocamento(id: 2, usuarioId: 2, deslocamentoId: 2, tipo: 1);
   PassageirosDeslocamentoDAO.save(segundoPassageirosDeslocamento);
-  final terceiroDeslocamento = Deslocamento(destinoId: 1, horaSaida: "14:00", id: 3, origemId: 1, status: 0, vagasDisponiveis: 10, veiculoId: null, vagas: 10);
-  DeslocamentoDAO.save(terceiroDeslocamento);
-  final terceiroPassageirosDeslocamento = PassageirosDeslocamento(id: 3, usuarioId: 2, deslocamentoId: 3, tipo: 1);
+  final terceiroPassageirosDeslocamento = PassageirosDeslocamento(id: 3, usuarioId: 3, deslocamentoId: 3, tipo: 1);
   PassageirosDeslocamentoDAO.save(terceiroPassageirosDeslocamento);
-  final quartoDeslocamento = Deslocamento(destinoId: 1, horaSaida: "21:00", id: 4, origemId: 1, status: 0, vagasDisponiveis: 4, veiculoId: 2, vagas: 4);
-  DeslocamentoDAO.save(quartoDeslocamento);
-  final quartoPassageirosDeslocamento = PassageirosDeslocamento(id: 4, usuarioId: 2, deslocamentoId: 4, tipo: 1);
+  final quartoPassageirosDeslocamento = PassageirosDeslocamento(id: 4, usuarioId: 4, deslocamentoId: 4, tipo: 1);
   PassageirosDeslocamentoDAO.save(quartoPassageirosDeslocamento);
-  final quintoDeslocamento = Deslocamento(destinoId: 1, horaSaida: "12:00", id: 5, origemId: 1, status: 0, vagasDisponiveis: 4, veiculoId: 2, vagas: 4);
-  DeslocamentoDAO.save(quintoDeslocamento);
-  final quintoPassageirosDeslocamento = PassageirosDeslocamento(id: 5, usuarioId: 2, deslocamentoId: 5, tipo: 1);
+  final quintoPassageirosDeslocamento = PassageirosDeslocamento(id: 5, usuarioId: 4, deslocamentoId: 5, tipo: 1);
   PassageirosDeslocamentoDAO.save(quintoPassageirosDeslocamento);
-  final sextoDeslocamento = Deslocamento(destinoId: 1, horaSaida: "11:00", id: 6, origemId: 1, status: 0, vagasDisponiveis: 4, veiculoId: 2, vagas: 4);
-  DeslocamentoDAO.save(sextoDeslocamento);
   final sextoPassageirosDeslocamento = PassageirosDeslocamento(id: 6, usuarioId: 2, deslocamentoId: 6, tipo: 1);
   PassageirosDeslocamentoDAO.save(sextoPassageirosDeslocamento);
-  final setimoDeslocamento = Deslocamento(destinoId: 2, horaSaida: "13:00", id: 7, origemId: 3, status: 0, vagasDisponiveis: 4, veiculoId: 2, vagas: 4);
-  DeslocamentoDAO.save(setimoDeslocamento);
-  final setimoPassageirosDeslocamento = PassageirosDeslocamento(id: 7, usuarioId: 2, deslocamentoId: 7, tipo: 1);
+  final setimoPassageirosDeslocamento = PassageirosDeslocamento(id: 7, usuarioId: 3, deslocamentoId: 7, tipo: 1);
   PassageirosDeslocamentoDAO.save(setimoPassageirosDeslocamento);
-  final oitavoDeslocamento = Deslocamento(destinoId: 2, horaSaida: "08:00", id: 8, origemId: 3, status: 0, vagasDisponiveis: 4, veiculoId: 2, vagas: 4);
-  DeslocamentoDAO.save(oitavoDeslocamento);
-  final oitavoPassageirosDeslocamento = PassageirosDeslocamento(id: 8, usuarioId: 2, deslocamentoId: 8, tipo: 1);
+  final oitavoPassageirosDeslocamento = PassageirosDeslocamento(id: 8, usuarioId: 1, deslocamentoId: 8, tipo: 1);
   PassageirosDeslocamentoDAO.save(oitavoPassageirosDeslocamento);
 
   // DeslocamentoDAO.findAll().then((value) => print(value));
